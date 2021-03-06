1f7fb7fd39d77a14119a6f01074a556b767068ba
Iván Ruiz Ruiz <58665260+ivanruizalcuadrado@users.noreply.github.com>
2021-03-06 12:07:57 +0100

    Update install-powercli.ps1

--- install-powercli.ps1
+++ install-powercli.ps1
@@ -2,5 +2,5 @@ write-host "Instalando Modulos para uso de VMWare"
 Install-Module -Name VMware.PowerCLI  -Scope allusers -Confirm:$false -Force
 Install-Module -Name Posh-SSH   -allowclobber -Scope allusers -Confirm:$false -Force
 Install-Module -Name SqlServer -Scope allusers
-Set-PowerCLIConfiguration -Scope User -ParticipateInCEIP $true
+Set-PowerCLIConfiguration -Scope User -ParticipateInCEIP $true  -Confirm:$false -Force
 new-item $PROFILE.AllUsersAllHosts -ItemType file -Force
