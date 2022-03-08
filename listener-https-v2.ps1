555e02409e8207580e65f2bd0d469c39d10705c3
Iván Ruiz Ruiz <58665260+ivanruizalcuadrado@users.noreply.github.com>
2022-03-08 10:45:06 +0100

    Update listener-https-v2.ps1

--- listener-https-v2.ps1
+++ listener-https-v2.ps1
@@ -1,7 +1,7 @@
 
 $IP = hostname -I | awk '{print $1}'
 $LocalIP = $IP 
-$Puerto =  "888"
+$Puerto =  "80"
 $URLListener = "http://" + $LocalIP + ":" + $Puerto + "/"
 
 # Crea un  listener en el pueto 8000
